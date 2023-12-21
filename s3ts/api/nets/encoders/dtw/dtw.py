ba1393160f1ed3553316306265f4b31f21c6e32f
mbisan <mbikandi@bcamath.org>
2023-12-21 13:03:57 +0100

    add dtw_compute_no_grad

--- s3ts/api/nets/encoders/dtw/dtw.py
+++ s3ts/api/nets/encoders/dtw/dtw.py
@@ -31,7 +31,22 @@ def dtw_compute_no_n(dtw: torch.Tensor, dist_grad: torch.Tensor, grad: torch.Ten
     grad[:,:,:] += grads.sum(dim=(-2, -1))
 
 @torch.jit.script
-def dtw_fast_no_n(x: torch.Tensor, y: torch.Tensor, w: float, eps: float = 1e-5):
+def dtw_compute_no_grad(dtw: torch.Tensor, w: float) -> None:
+    '''
+        dtw of shape (n, k, pattern_len, window_size)
+        grad of shape (n, k, dims, pattern_len)
+    '''
+
+    n, k, len_pattern, len_window = dtw.shape
+
+    for i in range(1, len_pattern): # pl
+        for j in range(1, len_window): # ws
+            value = torch.minimum(w * torch.minimum(dtw[:, :, i, j-1], dtw[:, :, i-1, j-1]), dtw[:, :, i-1, j])
+
+            dtw[:, :, i, j] += value
+
+@torch.jit.script
+def dtw_fast_no_n(x: torch.Tensor, y: torch.Tensor, w: float, eps: float = 1e-5, compute_gradients: bool=True):
     # shape of x (n, dim, x_len) y (m, dim, y_len)
 
     # performs convolution-like operation, for each kernel the DF
@@ -42,25 +57,30 @@ def dtw_fast_no_n(x: torch.Tensor, y: torch.Tensor, w: float, eps: float = 1e-5)
     p_diff = x[:,None,:,None,:] - y[None,:,:,:,None] # shape (n, n_kernel, d, Kernel_size, T)
     euc_d = torch.square(p_diff).sum(2) # shape (n, n_kernel, kernel_size, T)
 
-    # p_diff now contains the partial derivatives of DTW[n, k, i, j] wrt K[k, d, i] (dims (n, k, d, i, j))
-    p_diff = p_diff / torch.sqrt(euc_d[:,:, None, :, :] + eps)
-
     # compute dtw
     euc_d[:,:,0,:] = torch.cumsum(euc_d[:,:,0,:], dim=2)
     euc_d[:,:,:,0] = torch.cumsum(euc_d[:,:,:,0], dim=2)
 
-    grads = torch.zeros((x.shape[0], y.shape[0], y.shape[1], y.shape[2])) # dims (n, k, d, i)
+    if compute_gradients:
+        # p_diff now contains the partial derivatives of DTW[n, k, i, j] wrt K[k, d, i] (dims (n, k, d, i, j))
+        p_diff = p_diff / torch.sqrt(euc_d[:,:, None, :, :] + eps)
+        
+        grads = torch.zeros((x.shape[0], y.shape[0], y.shape[1], y.shape[2])) # dims (n, k, d, i)
 
-    futures = [torch.jit.fork(dtw_compute_no_n, euc_d[i], p_diff[i], grads[i], w) for i in range(x.shape[0])] 
-    results = [torch.jit.wait(future) for future in futures]
+        futures = [torch.jit.fork(dtw_compute_no_n, euc_d[i], p_diff[i], grads[i], w) for i in range(x.shape[0])] 
+        results = [torch.jit.wait(future) for future in futures]
+        
+        return euc_d.sqrt(), grads
+    else:
+        dtw_compute_no_grad(euc_d, w)
 
-    return euc_d.sqrt(), grads
+        return euc_d.sqrt(), None
 
 class torch_dtw(torch.autograd.Function):
 
     @staticmethod
-    def forward(x, y, w):
-        DTW, p_diff = dtw_fast_no_n(x, y, w)
+    def forward(x: torch.Tensor, y: torch.Tensor, w: float):
+        DTW, p_diff = dtw_fast_no_n(x, y, w, compute_gradients=y.requires_grad)
         return DTW, p_diff
     
     @staticmethod
@@ -73,4 +93,4 @@ class torch_dtw(torch.autograd.Function):
         # dtw_grad dims (n, k, i, j) p_diff dims (n, k, d, i)
         p_diff, = ctx.saved_tensors
         mult = (p_diff[:, :, :, :, None] * dtw_grad[:, :, None, :, :]) # dims (n, k, d, i, j)
-        return mult.mean(dim=(1, 3)), mult.mean(dim=(0, 4)), None
+        return None, mult.mean(dim=(0, 4)), None
