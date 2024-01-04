ea6d6def4d27553e3f0d4d69623b39e5036afb49
mbisan <mbikandi@bcamath.org>
2024-01-04 15:46:49 +0100

    update create jobs and naming of models

--- create_jobs.py
+++ create_jobs.py
@@ -1,33 +1,64 @@
 import os
 import numpy as np
 
-from s3ts.helper_functions import get_model_name, get_parser
+from s3ts.arguments import get_model_name, get_command
 
-class Experiments:
-    num_workers = 8
-    dataset = "HARTH"
-    subjects_for_test = [
+baseArguments = {
+    "num_workers": 8,
+    "dataset": "HARTH",
+    "subjects_for_test": [
         [21]
-    ]
-    epochs = 30
-    rho = 0.1
-    batch_size = 128
-    lr = 0.001
-    mode = "img"
-    encoder_architecture = "cnn_gap"
-    encoder_features = 20
-    decoder_architecture = "mlp"
-    decoder_features = 32
-    decoder_layers = 1
-    window_size = 48
-    window_stride = [1, 2]
-    num_medoids = 1
-    compute_n = 300
-    overlap = [-1, 45]
-    use_medoids = [True, False]
-    label_mode = 1
-    voting = 1
-    pattern_size = [16, 32, 48]
+    ],
+    "lr": 0.001,
+    "encoder_architecture": "cnn_gap",
+    "encoder_features": 20,
+    "decoder_architecture": "mlp",
+    "decoder_features": 32,
+    "decoder_layers": 1,
+    "window_size": 48,
+    "window_stride": 1,
+    "batch_size": 128,
+    "label_mode": 1,
+    "voting": 1,
+    "rho": 0.1,
+    "overlap": -1,
+    "max_epochs": 30
+}
+
+imgExperiments = {
+    "mode": "img",
+    "num_medoids": 1,
+    "compute_n": 300,
+    "use_medoids": [True, False],
+    "pattern_size": [16, 32, 48]
+}
+
+dtwExperiments = {
+    "mode": "dtw",
+    "pattern_size": [16, 32, 48]
+}
+
+dtwcExperiments = {
+    "mode": "dtw_c",
+    "pattern_size": [16, 32, 48]
+}
+
+tsExperiments = {
+    "mode": "ts"
+}
+
+gasfExperiments = {
+    "mode": "gasf"
+}
+
+gadfExperiments = {
+    "mode": "gadf"
+}
+
+mtffExperiments = {
+    "mode": "mtf",
+    "mtf_bins": 16
+}
 
 def create_jobs(args):
     modelname = get_model_name(args)
@@ -48,41 +79,38 @@ cd $HOME/s3ts
 source $HOME/.bashrc
 source activate dev
 
-python training.py --dataset {args.dataset} --window_size {args.window_size*2 if args.mode=="dtw" else args.window_size} --window_stride {args.window_stride} \\
---pattern_size {args.pattern_size} \\
---subjects_for_test {" ".join([str(subject) for subject in args.subjects_for_test])} \\
---encoder_architecture {args.encoder_architecture} --encoder_features {args.encoder_features} \\
---decoder_architecture {args.decoder_architecture} --decoder_features {args.decoder_features} --decoder_layers {args.decoder_layers} \\
---mode {args.mode} --max_epochs {args.epochs} \\
---batch_size {args.batch_size} --lr {args.lr} --num_workers {args.num_workers} \\
---reduce_imbalance --normalize --label_mode {args.label_mode} --num_medoids {args.num_medoids} --compute_n {args.compute_n} \\
---rho {args.rho} --voting {args.voting} --use_{"medoids" if args.use_medoids else "synthetic"} --overlap {args.overlap}
+python training.py {get_command(args)}
 '''
 
-if __name__ == "__main__":
-    if not os.path.exists("cache_jobs"):
-        os.mkdir(os.path.join("./", "cache_jobs"))
+class EmptyExperiment:
+    pass
+
+def produce_experiments(args):
+
+    print("Experiments for mode", args["mode"])
 
     multiple_arguments = []
-    for key, value in Experiments.__dict__.items():
+    for key, value in args.items():
         if "__" not in key:
             if isinstance(value, list):
                 multiple_arguments.append((key, len(value)))
 
     total_experiments = np.prod([it[1] for it in multiple_arguments])
-    total_experiments
 
-    parser = get_parser()
-    experiment_arguments = [parser.parse_args([]) for i in range(total_experiments)]
+    experiment_arguments = [EmptyExperiment() for i in range(total_experiments)]
+
+    for exp in experiment_arguments:
+        exp.__dict__.update(args)
 
     k = 1
-    for key, value in Experiments.__dict__.items():
+    for key, value in args.items():
         if "__" in key:
             continue
 
         if isinstance(value, list):
             n = len(value)
-            print("Argument with multiple values:", key, "with", n, "values")
+            if n>1:
+                print("Argument with multiple values:", key, "with", n, "values")
             for i, experiment_arg in enumerate(experiment_arguments):
                 setattr(experiment_arg, key, value[(i//k)%n])
             k *= n
@@ -98,6 +126,18 @@ if __name__ == "__main__":
         with open(os.path.join("./", "cache_jobs", jobname + ".job"), "w") as f:
             f.write(job)
 
+    print("Created", len(jobs), "jobs")
+
+    return jobs
+
+if __name__ == "__main__":
+    if not os.path.exists("cache_jobs"):
+        os.mkdir(os.path.join("./", "cache_jobs"))
+
+    jobs = []
+    for exp in [imgExperiments, dtwExperiments, dtwcExperiments, tsExperiments, gasfExperiments, gadfExperiments, mtffExperiments]:
+        jobs += produce_experiments({**baseArguments, **exp})
+    
     bash_script = "#!\\bin\\bash\n" + "\n".join(jobs)
     with open(os.path.join("./", "cache_jobs", "launch.sh"), "w") as f:
         f.write(bash_script)
---
+++ s3ts/arguments.py
@@ -0,0 +1,40 @@
+
+def get_model_name(args):
+    modelname = f"{args.mode}|{args.dataset}," + '-'.join([str(subject) for subject in args.subjects_for_test]) + "|" \
+                f"{args.window_size},{args.window_stride}|bs{args.batch_size}|" + \
+                f"{args.encoder_architecture}{args.encoder_features}|" + \
+                f"{args.decoder_architecture}{args.decoder_features},{args.decoder_layers}|" + \
+                (f"m{args.label_mode}|" if args.label_mode > 1 else "") + \
+                (f"v{args.voting},{args.rho}|" if args.voting > 1 else "") + \
+                (f"ov{args.overlap}|" if args.overlap > 0 else "")
+    
+    if args.mode in ["img", "dtw", "dtw_c"]:
+        modelname += f"p{args.pattern_size},r{args.rho}|"
+    if args.mode == "img":
+        modelname += f"med{args.num_medoids},{args.compute_n}|" if args.use_medoids else f"syn{args.compute_n}|"
+    if args.mode == "mtf":
+        modelname += f"bin{args.mtf_bins}|"
+
+    return modelname[:-1]
+
+def get_command(args):
+    command = f"--mode {args.mode} --dataset {args.dataset} " + \
+                "--subjects_for_test " + ' '.join([str(subject) for subject in args.subjects_for_test]) + " " \
+                f"--window_size {args.window_size} --window_stride {args.window_stride} --batch_size {args.batch_size} " + \
+                f"--encoder_architecture {args.encoder_architecture} --encoder_features {args.encoder_features} " + \
+                f"--decoder_architecture {args.decoder_architecture} --decoder_features {args.decoder_features} --decoder_layers {args.decoder_layers} " + \
+                (f"--label_mode {args.label_mode} " if args.label_mode > 1 else "") + \
+                (f"--voting {args.voting} --rho {args.rho} " if args.voting > 1 else "") + \
+                (f"--overlap {args.overlap} " if args.overlap > 0 else "")
+    
+    if args.mode in ["img", "dtw", "dtw_c"]:
+        command += f"--pattern_size {args.pattern_size} --rho {args.rho} "
+    if args.mode == "img":
+        command += f"--compute_n {args.compute_n} "
+        command += f"--use_medoids --num_medoids {args.num_medoids} " if args.use_medoids else f"--use_synthetic "
+    if args.mode == "mtf":
+        command += f"--mtf_bins {args.mtf_bins} "
+
+    command += f"--num_workers {args.num_workers} --max_epochs {args.max_epochs} --normalize --reduce_imbalance"
+
+    return command
