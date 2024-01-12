8c6a720fb288cabd2d4480a6c0684f40d92726c9
mbikandi <mbikandi@bcamath.org>
2024-01-12 09:55:09 +0100

    fix log

--- experiment_definition.py
+++ experiment_definition.py
@@ -11,18 +11,18 @@ baseArguments = {
     "decoder_architecture": "mlp",
     "decoder_features": 32,
     "decoder_layers": 1,
-    "window_size": 16,
-    "window_stride": [2, 4],
     "batch_size": 128,
     "label_mode": 1,
     "voting": 1,
     "rho": 0.1,
     "overlap": -1,
-    "max_epochs": 30,
-    "training_dir": "training_2"
+    "max_epochs": 15,
+    "training_dir": "training_new"
 }
 
 imgExperiments = {
+    "window_size": [16, 32, 48],
+    "window_stride": [1, 2, 4],
     "mode": "img",
     "num_medoids": 1,
     "compute_n": 300,
@@ -31,28 +31,40 @@ imgExperiments = {
 }
 
 dtwExperiments = {
+    "window_size": 48,
+    "window_stride": 1,
     "mode": "dtw",
     "pattern_size": [8, 16, 24]
 }
 
 dtwcExperiments = {
+    "window_size": 48,
+    "window_stride": 1,
     "mode": "dtw_c",
     "pattern_size": [8, 16, 24]
 }
 
 tsExperiments = {
+    "window_size": 48,
+    "window_stride": 1,
     "mode": "ts"
 }
 
 gasfExperiments = {
+    "window_size": 48,
+    "window_stride": 1,
     "mode": "gasf"
 }
 
 gadfExperiments = {
+    "window_size": 48,
+    "window_stride": 1,
     "mode": "gadf"
 }
 
 mtffExperiments = {
+    "window_size": 48,
+    "window_stride": 1,
     "mode": "mtf",
     "mtf_bins": 10
 }
@@ -60,4 +72,4 @@ mtffExperiments = {
 RAM = 32
 CPUS = 16
 
-experiments = [imgExperiments] #, dtwExperiments, dtwcExperiments, tsExperiments, gasfExperiments, gadfExperiments, mtffExperiments]
+experiments = [imgExperiments, dtwExperiments, dtwcExperiments, tsExperiments, gasfExperiments, gadfExperiments, mtffExperiments]
