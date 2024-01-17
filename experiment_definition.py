0eafcffbc41b95767eb31380cb79b91f3bf19f75
mbikandi <mbikandi@bcamath.org>
2024-01-17 11:18:16 +0100

    update experiments

--- experiment_definition.py
+++ experiment_definition.py
@@ -2,10 +2,31 @@ baseArguments = {
     "num_workers": 8,
     "dataset": "HARTH",
     "subjects_for_test": [
-        [21]
+        [21],
+	[20],
+	[19],
+	[18],
+	[17],
+	[16],
+	[15],
+	[14],
+	[13],
+	[12],
+	[11],
+	[10],
+	[9],
+	[8],
+	[7],
+	[6],
+	[5],
+	[4],
+	[3],
+	[2],
+	[1],
+	[0]
     ],
     "lr": 0.001,
-    "n_val_subjects": 5,
+    "n_val_subjects": 4,
     "encoder_architecture": "cnn_gap",
     "encoder_features": 20,
     "decoder_architecture": "mlp",
@@ -16,18 +37,18 @@ baseArguments = {
     "voting": 1,
     "rho": 0.1,
     "overlap": -1,
-    "max_epochs": 15,
-    "training_dir": "training_new"
+    "max_epochs": 10,
+    "training_dir": "training_cv2"
 }
 
 imgExperiments = {
-    "window_size": [16, 32, 48],
-    "window_stride": [1, 2, 4],
+    "window_size": 25,
+    "window_stride": 3,
     "mode": "img",
     "num_medoids": 1,
     "compute_n": 300,
-    "use_medoids": [True, False],
-    "pattern_size": [32, 48]
+    "use_medoids": False,
+    "pattern_size": 32
 }
 
 dtwExperiments = {
@@ -72,4 +93,4 @@ mtffExperiments = {
 RAM = 32
 CPUS = 16
 
-experiments = [imgExperiments, dtwExperiments, dtwcExperiments, tsExperiments, gasfExperiments, gadfExperiments, mtffExperiments]
+experiments = [imgExperiments] #, dtwExperiments, dtwcExperiments, tsExperiments, gasfExperiments, gadfExperiments, mtffExperiments]
